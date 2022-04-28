700b86ae6485021f48324f7dd2fa117e11058001
voncardoso <vonharrison26@gmail.com>
2022-04-27 23:50:39 -0300

    feat(feat(styles): implementing styles on the landing page): :lipstick: implementation of landing page intro styles
    implementation of landing page intro styles for both desktop and mobile

--- src/pages/LandingPage/index.js
+++ src/pages/LandingPage/index.js
@@ -1,10 +1,29 @@
 import Header from "../../components/Header";
 import style from "./style.module.css"
+import {ReactComponent as ImgPrincipalLP} from '../../assets/ImgPrincipalLP.svg'
+import {ReactComponent as ImgWavyEffect} from '../../assets/ImgWavyEffect.svg'
 const LandingPage = () =>{
     return(
         <>
-            <section className={style.introduction}>
+            <section className={style.nav}>
                 <Header/>
+
+                <section className={style.introduction}>
+                    <div className={style.titulo}>
+                        <span>Plataforma</span>
+                        <h1>Jogos Adaptaveis</h1>
+                        <p>Desevolvida para gera relatorios sobre o desenpenho dos jogadores </p>
+                    </div>
+
+                    <div className={style.Img}>
+                        <ImgPrincipalLP/>
+                    </div>
+
+                    <div className={style.ImgEffect}>
+                    </div>
+                    
+                </section>
+                
             </section>
             
         </>
