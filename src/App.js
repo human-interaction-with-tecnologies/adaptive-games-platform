f6d90265c479de5f0b6c916e7fda3eae7a360558
voncardoso <vonharrison26@gmail.com>
2022-05-19 22:36:12 -0300

    feat(create router): :bricks: login route creation
    creation of the main login route to later create its subroutes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import './globla.module.css'
-import LandingPage from './pages/LandingPage/index'
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import LandingPage from './pages/LandingPage/index';
+import './globla.module.css';
+import Login from './pages/Login';
 
 function App() {
   return (
-    <>
-      <LandingPage />
-    </>
+    <div>
+      <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<LandingPage />}/>
+        <Route path='/login/*' element={<Login />}/>
+      </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react';
+import {Link} from 'react-router-dom';
 import useMedia from '../../Hooks/useMedia';
 import styles from './style.module.css';
 
@@ -40,9 +41,7 @@ const Header = () =>{
                 <a href="#" >
                     Financiadores
                 </a>
-                <button className={styles.btnLoginHeader} onClick={()=>{
-                    setMobileMenu(false)
-                }}>Login</button>
+                <Link className={styles.btnLoginHeader} to='/login'>Login</Link>
             </nav>
         </>
     )
